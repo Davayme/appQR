d20d8db138f110256c70d9685252ec5622c5e83b
David Ayme <dayme1937@uta.edu.ec>
2024-09-19 14:58:15 -0500

    ultimo

--- src/app/qr/qr.module.ts
+++ src/app/qr/qr.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { QrRoutingModule } from './qr-routing.module';
+import { PageListComponent } from './page-list/page-list.component';
+import { FormComponent } from './form/form.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PageListComponent,
+    FormComponent
+  ],
   imports: [
     CommonModule,
     QrRoutingModule

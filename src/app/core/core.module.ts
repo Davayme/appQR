b4b805965ce3a5d0b27ece1331c9432d97070b9d
David Ayme <dayme1937@uta.edu.ec>
2024-09-19 15:36:25 -0500

    Add Angular Material modules to core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,9 +4,14 @@ import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
 import { PageLoginComponent } from './page-login/page-login.component';
 import { LoginComponent } from './login/login.component';
-
-
-
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -17,7 +22,14 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     CommonModule,
-  
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatTooltipModule,
+    MatToolbarModule,
+    MatListModule
   ]
 })
 export class CoreModule { }

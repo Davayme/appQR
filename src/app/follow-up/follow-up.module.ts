b91030541d16d31dbfe3a6b9c0226bc3c5a23023
David Ayme <dayme1937@uta.edu.ec>
2024-09-19 14:56:43 -0500

    Add form and page-list components for agency, client, channel, and employee modules

---
+++ src/app/follow-up/follow-up.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FollowUpRoutingModule } from './follow-up-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FollowUpRoutingModule
+  ]
+})
+export class FollowUpModule { }
